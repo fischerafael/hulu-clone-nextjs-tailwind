d6b06648261ec34aa582ae96f7247dbdbfcd5526
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-06-16 16:54:09 -0300

    add nav scroll bar

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,13 +1,24 @@
-import { requests } from '../../utils/requests'
+import { categories } from '../../utils/requests'
 
 export const Nav = () => {
     return (
-        <nav>
-            <div>
-                {Object.entries(requests).map(([key, { title, url }]) => (
-                    <h1>{title}</h1>
+        <nav className="bg-background p-2">
+            <div className="w-500 gap-10 flex flex-row no-scrollbar justify-start overflow-x-scroll overflow-y-hidden overscroll-contain">
+                {categories.map((category, index) => (
+                    <NavLink key={index} category={category} />
                 ))}
             </div>
         </nav>
+    )
+}
+
+const NavLink = ({ category }) => {
+    return (
+        <h1
+            className="text-white cursor-pointer transform hover:scale-110 hover:text-gray-600 duration-200 active:text-red-500"
+            key={category.title}
+        >
+            {category.title}
+        </h1>
     )
 }
--- utils/requests.ts
+++ utils/requests.ts
@@ -3,6 +3,33 @@ const API_KEY = process.env.API_KEY
 export const requests = {
     discover: {
         title: 'DISCOVER',
-        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+        url: `http://www.omdbapi.com/?apikey=${API_KEY}&`
     }
 }
+
+export const categories = [
+    {
+        title: 'Descubra',
+        url: '#'
+    },
+    {
+        title: 'Ação',
+        url: '#'
+    },
+    {
+        title: 'Comédia',
+        url: '#'
+    },
+    {
+        title: 'Drama',
+        url: '#'
+    },
+    {
+        title: 'Animação',
+        url: '#'
+    },
+    {
+        title: 'Infantil',
+        url: '#'
+    }
+]
